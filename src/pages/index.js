3fe145cef01a8bdcf6e277b8d769603a47806f91
sateesh <sateeshkumarp3113@gmail.com>
2025-04-19 18:25:02 +0530

    bug fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import ProfilePage from "./ProfilePage";
 import SavedFilesPage from "./SavedFilesPage";
 import SettingsPage from "./SettingsPage";
 import SignUpPage from "./SignUpPage";
-import Callback from "./CallBack";
+// import Callback from "./CallBack";
 import VerifyEmailPage from "./VerifyEmailPage";
 
-export {ExecutionHistoryPage, HomePage, LogInPage, ProfilePage, SavedFilesPage, SettingsPage, SignUpPage, Callback, VerifyEmailPage}
+export {ExecutionHistoryPage, HomePage, LogInPage, ProfilePage, SavedFilesPage, SettingsPage, SignUpPage, VerifyEmailPage}
